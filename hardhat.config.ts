cc0b9de9c1dfd186affedd0723c41f704fde6095
SonOfMosiah <84041746+SonOfMosiah@users.noreply.github.com>
2022-11-22 00:05:32 -0700

    Update dependencies

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,32 +1,12 @@
+import { HardhatUserConfig } from 'hardhat/config';
+import '@nomicfoundation/hardhat-toolbox';
+import '@nomiclabs/hardhat-solhint';
+import 'hardhat-docgen';
 import * as dotenv from "dotenv";
-
-import "@nomiclabs/hardhat-etherscan";
-import "@nomiclabs/hardhat-waffle";
 import { } from "@openzeppelin/hardhat-upgrades";
-import "@typechain/hardhat";
-import "hardhat-gas-reporter";
-import { HardhatUserConfig, task } from "hardhat/config";
-import "solidity-coverage";
 
 require('@openzeppelin/hardhat-upgrades');
-
 dotenv.config();
-
-// This is a sample Hardhat task. To learn how to create your own go to
-// https://hardhat.org/guides/create-task.html
-task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
-    const accounts = await hre.ethers.getSigners();
-
-    for (const account of accounts) {
-        console.log(account.address);
-    }
-});
-
-// You need to export an object to set up your config
-// Go to https://hardhat.org/config/ to learn more
-
-
-const TEST_MNEMONICS = String(process.env.TEST_MNEMONICS)
 
 const config: HardhatUserConfig = {
 
@@ -141,7 +121,12 @@ const config: HardhatUserConfig = {
     },
     mocha: {
         parallel: false
-    }
+    },
+    docgen: {
+        path: './docs',
+        clear: true,
+        runOnCompile: true,
+    },
 };
 
 export default config;
