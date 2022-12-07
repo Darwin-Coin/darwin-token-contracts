afb9b70ea540222d442204d5d222c80edb52ceea
kingofclubstroy <100493790+kingofclubstroyDev@users.noreply.github.com>
2022-12-07 18:35:33 -0500

    Added a new version of the darwin token, needs to be tested
    Replaced the reflections fora much simpler system, inherits from erc20 and abstracted away the tokenomics 2.0 contract to be inherited from

---
+++ contracts/interface/IDarwinNew.sol
@@ -0,0 +1,63 @@
+pragma solidity 0.8.14;
+
+// SPDX-License-Identifier: Unlicensed
+
+interface IDarwinNew {
+
+    /// @notice Accumulatively log sold tokens
+    struct TokenSellLog {
+        uint40 lastSale;
+        uint216 amount;
+    }
+
+    event ExchangeAdded(address account);
+    event ExchangedRemoved(address account);
+
+    event TokenReflection(uint256 amount);
+
+    event ExcludedFromReflection(address account, bool isExcluded);
+    event ExcludedFromSellLimit(address account, bool isExcluded);
+
+    //TODO: make sure these are implemented
+
+    // function syncTokenInOutOfSyncExchangesSafe() external;
+
+    // function bulkTransfer(address[] calldata recipients, uint256[] calldata amounts) external;
+
+    // function getOutOfSyncedAmount(address pair) external view returns (uint256);
+
+    // function getOutOfSyncedPairs() external view returns (address[] memory);
+
+    // function isExchangeAddress(address account) external view returns (bool);
+
+    // function isExcludedFromReward(address account) external view returns (bool);
+
+    // function isExcludedFromTxLimit(address account) external view returns (bool);
+
+    // function isExcludedFromHoldingLimit(address account) external view returns (bool);
+
+    /// TransferFrom amount is greater than allowance
+    error InsufficientAllowance();
+    /// Only the DarwinCommunity can call this function
+    error OnlyDarwinCommunity();
+    /// rAmount is greater than the total reflections
+    error RAmountGreaterThanReflections();
+    /// Input cannot be the zero address
+    error ZeroAddress();
+    /// Amount cannot be 0
+    error ZeroAmount();
+    /// Arrays must be the same length
+    error InvalidArrayLengths();
+    /// Pair is already registered
+    error PairAlreadyRegistered();
+    /// Pair is not registered
+    error PairNotRegistered();
+    /// Holding limit exceeded
+    error HoldingLimitExceeded();
+    /// Sell limit exceeded
+    error SellLimitExceeded();
+    /// Paused
+    error Paused();
+    error AccountAlreadyExcluded();
+    error AccountNotExcluded();
+}
---
+++ contracts/interface/ITokenomics2.sol
@@ -0,0 +1,45 @@
+pragma solidity 0.8.14;
+
+// SPDX-License-Identifier: Unlicensed
+
+interface ITokenomics {
+
+    event ExchangeAdded(address account);
+    event ExchangedRemoved(address account);
+
+    event TokenReflection(uint256 amount);
+
+    event ExcludedFromReflection(address account, bool isExcluded);
+    event ExcludedFromSellLimit(address account, bool isExcluded);
+
+    //TODO: make sure these are implemented
+
+    // function syncTokenInOutOfSyncExchangesSafe() external;
+
+    // function bulkTransfer(address[] calldata recipients, uint256[] calldata amounts) external;
+
+    // function getOutOfSyncedAmount(address pair) external view returns (uint256);
+
+    // function getOutOfSyncedPairs() external view returns (address[] memory);
+
+    // function isExchangeAddress(address account) external view returns (bool);
+
+    // function isExcludedFromReward(address account) external view returns (bool);
+
+    // function isExcludedFromTxLimit(address account) external view returns (bool);
+
+    // function isExcludedFromHoldingLimit(address account) external view returns (bool);
+
+    /// Pair is already registered
+    error PairAlreadyRegistered();
+    /// Pair is not registered
+    error PairNotRegistered();
+    /// Holding limit exceeded
+    error HoldingLimitExceeded();
+    /// Sell limit exceeded
+    error SellLimitExceeded();
+    /// Paused
+    error Paused();
+    error AccountAlreadyExcluded();
+    error AccountNotExcluded();
+}
